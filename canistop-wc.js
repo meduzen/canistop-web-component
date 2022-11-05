1a34a9b49b3ec506536bbe668ea959f2983398a7
Mehdi M <hi@mehdi.cc>
2022-11-05 23:16:34 +0100

    Use slot and change various stuff

--- canistop-wc.js
+++ canistop-wc.js
@@ -3,61 +3,70 @@ class CanIStop extends HTMLElement {
     #$dataWrapper
 
     constructor() {
-        console.log('canistop constructor')
         super()
+
+        this.attachShadow({
+            mode: 'open',
+            // delegatesFocus: true,
+            // slotAssignment: 'manual',
+        })
+
+        this.shadowRoot.innerHTML = `
+            <style>
+                * { color: green; }
+            </style>
+            <slot>default slot content</slot>
+        `
     }
 
     connectedCallback() {
-        console.log('canistop connectedCallback')
-        this.#fetchData()
+        this.#fetch()
 
         this.#$dataWrapper = document.createElement('div')
-        this.appendChild(this.#$dataWrapper)
+        this.shadowRoot.appendChild(this.#$dataWrapper)
 
         const refreshButton = document.createElement('button')
         refreshButton.type = 'button'
         refreshButton.textContent = this.getAttribute('refresh-button') || 'refresh'
-        this.appendChild(refreshButton)
+        refreshButton.addEventListener('click', this.#fetch.bind(this))
+        this.shadowRoot.appendChild(refreshButton)
 
-        refreshButton.addEventListener('click', this.#fetchData.bind(this))
+        const footer = document.createElement('div')
+        footer.innerHTML = `data: <a href="https://canistop.net">canistop.net</a>`
+        this.shadowRoot.appendChild(footer)
     }
 
-    disconnectedCallback() {
-        console.log('canistop disconnectedCallback')
-    }
+    // disconnectedCallback() {
+    //     console.log('canistop disconnectedCallback')
+    // }
 
-    attributeChangedCallback() {
-        console.log('canistop attributeChangedCallback')
-    }
+    // attributeChangedCallback() {
+    //     console.log('canistop attributeChangedCallback')
+    // }
 
-    #fetchData() {
-        console.log('fetchData')
+    #fetch() {
         if (this.#fetching) { return }
+
         const browser = this.getAttribute('browser')
         this.#fetching = true
         fetch(`https://canistop.net/storage/data/${browser}.json`)
             .then(data => data.json())
             .then(data => {
-                console.log(data)
-                const regions = this.getAttribute('regions')
-                    .split(',')
-                    .map(region => ({
-                        region: region,
-                        usage: data.usage[region],
-                    }))
+                var regions =
+                    (this.getAttribute('regions'))?.split(',')
+                    ?? Object.keys(data.usage)
 
-                const date = data.date
-                const list = regions
-                    .map(region => `<li>${region.region}: ${region.usage * 100}%</li>`)
+                const usage = regions
+                    .map(region => `<li>${region}: ${data.usage[region] * 100}%</li>`)
                     .join('')
 
                 this.#$dataWrapper.innerHTML = `
-                    <h2>${data.browser} ${data.version} in ${date}</h2>
-                    <ul>${list}</ul>
+                    <h2>${data.browser} ${data.version} in ${data.date}</h2>
+                    <ul>${usage}</ul>
                 `
             })
             // Should we kill the whole component, including its surrounding DOM?
-            .catch(() => console.error('erf'))
+            .catch(err => console.error(err))
             .finally(() => this.#fetching = false)
     }
 }
