3d57cc38fec858d4a1e22a0f4b0db42e92b27ca2
Mehdi M <hi@mehdi.cc>
2022-11-12 23:54:52 +0100

    Start playing with styles

--- canistop-wc.js
+++ canistop-wc.js
@@ -19,10 +19,25 @@ const template = `
       ::slotted(.slot-outside-style) {
         background-color: olive;
       }
+
+      .refresh {
+        padding: .4rem;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        gap: .5rem;
+      }
   </style>
   <div id="data-wrapper">something</div>
   <slot>inner slot content</slot>
-  <button id="refresh" type="button">refresh</button>
+  <button class="refresh" id="refresh" type="button">
+      <span id="refresh-label">refresh</span>
+      <svg class="refresh-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 58 50" width="22" height="19">
+          <path fill="none" stroke="currentColor" stroke-width="5.983" stroke-linecap="round" stroke-linejoin="round" d="M54.038 21.949l-8.74 11.122-10.392-9.818"/>
+          <path fill="currentColor" d="M41.127 42.89a2.992 2.992 0 0 0-4.23-4.23l4.23 4.23zM3.114 28.809l-2.977.297.01.098.016.096 2.95-.491zM30.037 4.877l-.86 2.865.86-2.865zm11.966 20.94a2.991 2.991 0 1 0 5.983 0h-5.983zM36.897 38.66C26.625 48.931 8.65 43.83 6.065 28.317L.163 29.3c3.398 20.386 27.304 27.25 40.964 13.59l-4.23-4.23zM6.09 28.511C4.765 15.255 16.647 3.983 29.178 7.742l1.719-5.73C13.513-3.205-1.529 12.446.137 29.105l5.954-.595zM29.178 7.742c6.508 1.952 12.825 8.986 12.825 18.075h5.983c0-11.85-8.166-21.129-17.089-23.806l-1.72 5.731z"/>
+      </svg>
+  </button>
+
   <div>data: <a href="https://canistop.net">canistop.net</a></div>
 `
 
@@ -59,6 +74,7 @@ class CanIStopElement extends HTMLElement {
 
   #$dataWrapper
   #$refreshBtn
+  #$refreshBtnLabel
 
   constructor() {
     super()
@@ -71,6 +87,7 @@ class CanIStopElement extends HTMLElement {
     root.innerHTML = template
     this.#$dataWrapper = root.getElementById('data-wrapper')
     this.#$refreshBtn = root.getElementById('refresh')
+    this.#$refreshBtnLabel = root.getElementById('refresh-label')
     this.#$refreshBtn.addEventListener('click', this.#fetch.bind(this))
   }
 
@@ -88,7 +105,7 @@ class CanIStopElement extends HTMLElement {
 
     const label = this.getAttribute('refresh-label')
     if (label) {
-      this.#$refreshBtn.innerHTML = label
+      this.#$refreshBtnLabel.innerHTML = label
     }
   }
 
@@ -140,13 +157,20 @@ class CanIStopElement extends HTMLElement {
 
         const usage = regions
           .map(region =>
-              `<li>${region}: ${round(data.usage[region], 2).toFixed(2)}%</li>`
+              `<tr>
+                  <th>${region}</th>
+                  <td>${round(data.usage[region], 2).toFixed(2)}%</td>
+              </tr>`
           )
           .join('')
 
         this.#$dataWrapper.innerHTML = `
-          <h2>${data.browser} ${data.version} usage in ${data.date}</h2>
-          <ul>${usage}</ul>
+          <table>
+              <thead>
+                  <tr><th colspan="2">${data.browser} ${data.version} usage in ${data.date}</th></tr>
+              </thead>
+              <tbody>${usage}</tbody>
+          </table>
         `
 
         this.dispatchEvent(new Event('canistop:updated'))
